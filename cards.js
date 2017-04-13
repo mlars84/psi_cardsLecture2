dafb7b6cdb2161cb62fdca5a887b3b2c51cfe676
Matt Larson <matt.a.larson@gmail.com>
2017-04-13 09:45:35 -0500

    first

--- cards.js
+++ cards.js
@@ -2,7 +2,7 @@ var deck = [];
 
 $( document ).ready( function(){
   $( '#addCardButton' ).on( 'click', function(){
-    console.log( 'in addCardButton on click');
+    // console.log( 'in addCardButton on click');
     // create new card using the cardyCard constructor (below)
     var newCard = new cardyCard( $( '#numberIn' ).val(), $( '#suitIn' ).val() );
     // push the new card into the deck
@@ -17,11 +17,33 @@ $( document ).ready( function(){
     } // end for
   }); // end function
 
+
   // card constructor
-  function cardyCard( myNumber, mySuit ){
-    console.log( 'in cardyCard:', myNumber, mySuit );
+  function cardyCard( myNumber, mySuit, myValue ){
+    // console.log( 'in cardyCard:', myNumber, mySuit );
     // create a new object with the following properties:
     this.number = myNumber;
     this.suit = mySuit;
+    this.value = myValue;
   } // end cardyCard
+
+
+// attempt at building full deck generator
+  function wholeDeck(){
+
+    var emptyDeck = [];
+
+    this.names = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+    this.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+
+    // loops to loop through names and suits
+    for (var i = 0; i < this.suits.length; i++) {
+      for (var j = 0; j < this.names.length; j++) {
+      emptyDeck.push(this.names[i], this.suits[j]); // working on pushing loops into emptyDeck
+      }
+    }
+    return emptyDeck;
+  }
+
+  console.log(wholeDeck()); // it is currently doubling the total cards (104) in the array and returning extraneous undefined
 }); // end doc ready
